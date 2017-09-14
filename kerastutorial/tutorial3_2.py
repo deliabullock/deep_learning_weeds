ae3c808b3faca97363f476523627b36f858b853a
Delia Bullock <delia.m.bullock@gmail.com>
2017-09-14 00:45:53 +0000

    tutorial to reconstruct pics

---
+++ kerastutorial/tutorial3_2.py
@@ -0,0 +1,92 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import cv2
+import pickle
+
+
+images = []
+image_info = pickle.load( open( "./data/test_picture_info.pkl", "rb" ) )
+for x in image_info:
+	if len(x["weeds"]) == 0:
+		continue
+	for y in x["weeds"]:
+		print y
+		images.append(cv2.imread(y))
+	for y in x["nonweeds"]:
+		print y
+		images.append(cv2.imread(y))
+	break
+
+
+# dimensions of our images.
+img_width, img_height = 299, 299
+
+train_data_dir = '../datacollection/data/train'
+validation_data_dir = '../datacollection/data/validate'
+nb_train_samples = 18535#2000
+nb_validation_samples = 7870#800
+epochs = 1#50
+batch_size = 16
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+output = model.predict(images, verbose=1)
+print output
+
+model.save_weights('first_try.h5')
