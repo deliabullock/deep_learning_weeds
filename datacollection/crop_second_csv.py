5d456ab7a7dc3b6be0440e9f5a31f6e6ca3c3ef7
Delia Bullock <delia.m.bullock@gmail.com>
2017-11-02 02:13:09 +0000

    making data from second csv

--- datacollection/crop_second_csv.py
+++ datacollection/crop_second_csv.py
@@ -2,12 +2,14 @@ import csv
 import pandas as pd
 import numpy as np
 import ast
-from PIL import Image
+from PIL import Image,ImageDraw
 import requests
 from io import BytesIO
 import random
 import pickle
 from green_pic import green_pic
+from keras.models import load_model
+from keras.preprocessing import image
 
 IMAGE_SIZE = 299
 
@@ -39,7 +41,7 @@ class csvreader(object):
             csv_data = pd.read_csv(f)
             urls = csv_data[['Input.URLimage']].as_matrix()
             
-            for i in range(len(url)):
+            for i in range(len(urls)):
                 arr2 = urls[i]
                 url = arr2[0]
                 if url not in images:
@@ -52,7 +54,7 @@ class csvreader(object):
 	for x in rand_indices:
 		images_2.append(images[x])
         
-    	f = open(url_pkl, 'wb')
+    	f = open(self.url_pkl, 'wb')
     	pickle.dump(images_2, f)
     	f.close()
 
@@ -62,27 +64,29 @@ class csvreader(object):
 	    print(i)
 	    i += 1
             imagenum = self.crop_image(url,imagenum)
-        f = open(weed_pkl, 'ab')
-        pickle.dump(weed_nums, f)
+        f = open(self.weed_pkl, 'ab')
+        pickle.dump(self.weed_nums, f)
         f.close()
-        f = open(nonweed_pkl, 'ab')
-        pickle.dump(nonweed_nums, f)
+        f = open(self.nonweed_pkl, 'ab')
+        pickle.dump(self.nonweed_nums, f)
         f.close()
-        f = open(x_y_pkl, 'ab')
-        pickle.dump(out_x_y, f)
+        f = open(self.x_y_pkl, 'ab')
+        pickle.dump(self.out_x_y, f)
         f.close()
 
     def crop_image(self, url, imagenum):
             def get_top_y(elem):
                 return elem['ys']['top']
 
-            response = requests.get(url)
+	    ip = 'http://128.84.3.178'
+            url_2 = str(ip + url[25:])
+            response = requests.get(url_2)
             im = Image.open(BytesIO(response.content))
 	    draw = ImageDraw.Draw(im,"RGBA")
             w, h = im.size
             rand_x = random.randrange(w)
             rand_y = random.randrange(h)
-	    out_x_y.append([url, rand_x, rand_y)
+	    self.out_x_y.append([url, rand_x, rand_y])
 	    print('('+url+', '+str(rand_x)+', '+str(rand_y)+')')
 
             x_start = rand_x % IMAGE_SIZE
@@ -97,8 +101,10 @@ class csvreader(object):
             for r in range(num_rows):
                 curr_x = x_start
                 for n in range(num_colu):
-			img = im.crop((curr_x, curr_y, curr_x + 298, curr_y + 298))
-			weed_pictures.append([img, curr_x, curr_y])
+			croppedim = im.crop((curr_x, curr_y, curr_x + 298, curr_y + 298))
+			croppedim.save('./data_csv_2/img'+str(imagenum)+'.jpg')
+			img = image.load_img('./data_csv_2/img'+str(imagenum)+'.jpg', target_size=(299, 299))
+			weed_pictures.append([croppedim, curr_x, curr_y, imagenum])
 
                 	x = image.img_to_array(img)
                 	x = np.expand_dims(x, axis=0)
@@ -107,23 +113,24 @@ class csvreader(object):
                 	else:
                         	weed_images = np.vstack([weed_images, x])
                     	curr_x += IMAGE_SIZE
+			imagenum+=1
                 curr_y += IMAGE_SIZE
 
 	    weed_images = (1./255)*weed_images
-	    weed_classes = model.predict_classes(weed_images, batch_size=10)
-            for i in len(weed_pictures):
+	    weed_classes = self.model.predict_classes(weed_images, batch_size=10)
+            for i in range(len(weed_pictures)):
 		croppedim = weed_pictures[i][0]
 		x = weed_pictures[i][1]
 		y = weed_pictures[i][2]
+		num = weed_pictures[i][3]
 		if weed_classes[i] == 1:
-                        croppedim.save(weed_dir + 'img'+str(imagenum)+'.jpg')
-			weed_nums.add('img'+str(imagenum)+'.jpg')
+                        croppedim.save(self.weed_dir + 'img'+str(num)+'.jpg')
+			self.weed_nums.add('img'+str(num)+'.jpg')
 			draw.polygon([(x, y),(x + 298, y),(x + 298, y + 298),( x, y + 298)], (150,0,255,50))
 		else:
-                        croppedim.save(nonweed_dir + 'img'+str(imagenum)+'.jpg')
-			nonweed_nums.add('img'+str(imagenum)+'.jpg')
+                        croppedim.save(self.nonweed_dir + 'img'+str(num)+'.jpg')
+			self.nonweed_nums.add('img'+str(num)+'.jpg')
 			draw.polygon([(x, y),(x + 298, y),(x + 298, y + 298),( x, y + 298)], (20,30,50,50))
-		imagenum+=1
 	    img_name = url[url.rfind('/')+1:]
             im.save('./data_csv_2/' + img_name)
             return imagenum
