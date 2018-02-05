67a1774549b0a5b2c792d3c89b39edfb12d8d89f
Delia Bullock <delia.m.bullock@gmail.com>
2018-02-05 18:40:52 +0000

    making sure visualize_sliding_window.py up to date

--- kerastutorial/visualize_sliding_window.py
+++ kerastutorial/visualize_sliding_window.py
@@ -8,6 +8,7 @@ from keras.models import load_model
 from keras.preprocessing import image
 from keras.optimizers import SGD
 
+model = load_model('../kerastutorial/my_model_c_23_just_incorrect_best.h5')
 def make_image(x_y_list, output, url):
         response = requests.get(url)
         im = Image.open(BytesIO(response.content))
@@ -25,28 +26,31 @@ def make_image(x_y_list, output, url):
 	
 	print('YEAHH')
         img_name = url[url.rfind('/')+1:] 
-	im.save(img_name)
+	im.save('pics_out_train/' + img_name)
 	print(img_name)
 
 def test():
-	image_info = pickle.load( open( "./data/validate_picture_info.pkl", "rb" ) )
+	image_info = pickle.load( open( "./data/train_picture_info.pkl", "rb" ) )
+	i = 0
 	for x in image_info:
-		if len(x["weeds"]) != 0:
+		if i < 90:
+			i += 1
 			continue
 		url = x["url"]
-		ip = 'http://128.84.3.178/ethan'
-        	url = str(ip + url[25:])
 		output = get_output(url)
 		make_image(output[0], output[1], url)
 		#break
 
 def get_output(url):
-	out = []
-	model = load_model('../kerastutorial/my_model_2_ballanced.h5')
-	weed_images = np.empty((1,1))
+	print('in get_output')
         response = requests.get(url)
         im = Image.open(BytesIO(response.content))
 	w, h = im.size
+	out = []
+	num_images_y = ((h - 299)/75) + 1
+	num_images_x = ((w - 299)/75) + 1
+	weed_images = [None] * (num_images_y*num_images_x)
+	i = 0
 	x = 0
 	y = 0
 	step = 75
@@ -57,18 +61,23 @@ def get_output(url):
 			out.append([x, y])
 			pic = image.img_to_array(img)
 			pic = np.expand_dims(pic, axis=0)
-			if weed_images.shape == (1,1):
-				weed_images = np.vstack([pic])
-			else:
-				weed_images = np.vstack([weed_images, pic])
+			weed_images[i] = pic
+			i += 1
+			#if weed_images.shape == (1,1):
+			#	weed_images = np.vstack([pic])
+			#else:
+			#	weed_images = np.vstack([weed_images, pic])
 			x = x + step
 		y = y + step
 			
+	weed_images = np.concatenate(weed_images, axis=0)
 	weed_images = (1./255)*weed_images
-	print(model.predict(weed_images, batch_size=32, verbose=0))
-	nonweed_classes = model.predict_classes(weed_images, batch_size=10)
-	print(len(nonweed_classes))
-	print(nonweed_classes)
+	#print(model.predict(weed_images, batch_size=32, verbose=0))
+	print('about to predict')
+	nonweed_classes = model.predict_classes(weed_images, batch_size=32)
+	print('finished predicting')
+	#print(len(nonweed_classes))
+	#print(nonweed_classes)
 	return [out, nonweed_classes]
 
 
