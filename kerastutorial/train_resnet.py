6de6d95cf4a67967dcd2ada3c0bbed00c4fa961a
Delia Bullock <delia.m.bullock@gmail.com>
2018-02-18 09:42:05 +0000

    adding resnet training code

---
+++ kerastutorial/train_resnet.py
@@ -0,0 +1,144 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import backend as K
+from keras.layers.normalization import BatchNormalization
+from collections import Counter
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+from keras.optimizers import SGD
+import time
+start = time.time()
+
+
+
+
+#dimensions of our images.
+img_width, img_height = 299, 299
+
+train_data_dir = '../datacollection/data_for_resnet/train'
+validation_data_dir = '../datacollection/data_for_resnet/validate'
+test_data_dir = '../datacollection/data_for_resnet/test'
+nb_train_samples = 37402#17544#6541
+nb_validation_samples = 7878#2208#1152
+epochs = 2
+batch_size = 32
+lrate = 0.01 ### HERE
+decay = lrate/epochs
+sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+#sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3) 
+
+
+#create the base pre-trained model
+
+
+base_model = InceptionV3(weights='imagenet', include_top=False)
+
+# add a global spatial average pooling layer
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+# let's add a fully-connected layer
+x = Dense(1024, activation='relu')(x)
+# and a logistic layer -- let's say we have 200 classes
+predictions = Dense(1, activation='softmax')(x)
+
+# this is the model we will train
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# first: train only the top layers (which were randomly initialized)
+# i.e. freeze all convolutional InceptionV3 layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+# compile the model (should be done *after* setting layers to non-trainable)
+model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        channel_shift_range=0.1,
+        rotation_range=180,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+## find class weights
+counter = Counter(train_generator.classes)
+max_val = float(max(counter.values()))
+class_weights = {class_id : max_val/num_images for class_id, num_images in counter.items()}
+print(class_weights)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    class_weight=class_weights,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+end = time.time()
+print(end - start)
+
+
+# train the model on the new data for a few epochs
+#model.fit_generator(...)
+
+# at this point, the top layers are well trained and we can start fine-tuning
+# convolutional layers from inception V3. We will freeze the bottom N layers
+# and train the remaining top layers.
+
+# let's visualize layer names and layer indices to see how many layers
+# we should freeze:
+for i, layer in enumerate(base_model.layers):
+   print(i, layer.name)
+
+# we chose to train the top 2 inception blocks, i.e. we will freeze
+# the first 249 layers and unfreeze the rest:
+for layer in model.layers[:249]:
+   layer.trainable = False
+for layer in model.layers[249:]:
+   layer.trainable = True
+
+# we need to recompile the model for these modifications to take effect
+# we use SGD with a low learning rate
+from keras.optimizers import SGD
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='binary_crossentropy', metrics=['accuracy'])
+#model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
+
+# we train our model again (this time fine-tuning the top 2 inception blocks
+# alongside the top Dense layers
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=3,
+    class_weight=class_weights,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
